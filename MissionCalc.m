bbb1418d1887f4e879b166370615cbeee0c61bba
jcasell <jcasell@purdue.edu>
2022-02-06 18:09:03 -0500

    Update MissionCalc.m

--- MissionCalc.m
+++ MissionCalc.m
@@ -1,9 +1,10 @@
-function [Science, Cost, Mass] = MissionCalc(candidateArchitecture)
+function [Science, Cost, Mass, ttHP] = MissionCalc(candidateArchitecture)
 
 %Calculate Instrument Package and Science
 [sci_instr, cost_instr, m_instr, power_instr] = Instrumentation(candidateArchitecture)
 
 %Calculate Telemetry Data Rate
+
 
 %Calculate Total Spacecraft Mass
 %Calculated using Planetary Mission SMAD table A-1
@@ -17,10 +18,13 @@ power_spacecraft = power_instr / 0.22;
 
 %Calculate Trajectory
 
-
+tthp = 0;
 
 %Total Science
+Science = 0;
 
 %Total Cost
+Cost = 0;
 
 %Return Risk
+Risk = 0;
