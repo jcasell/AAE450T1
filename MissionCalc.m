356a95231944f841b66a132676a8678008754a6b
jcasell <jcasell@purdue.edu>
2022-02-06 17:51:49 -0500

    Fixed Structure

--- MissionCalc.m
+++ MissionCalc.m
@@ -1,15 +1,7 @@
-function [Science, Cost, Mass] = MissionCalc(mission)
-ComNet = mission(1); %Communication Network Choice
-Prop = mission(2);   %Propulsion Choice
-Power = mission(3);  %Power Source Choice
-Instr = mission(4);    %Instrumentation Choice
-Traj = mission(6); %Orbital Maneuver Choice
-Craft = mission(6);    %Number of Spacecraft Choice
-LaunchV = mission(7);    %Launch Vehicle Choice
-Kick = mission(8);   %Kick Stages Choice
+function [Science, Cost, Mass] = MissionCalc(candidateArchitecture)
 
 %Calculate Instrument Package and Science
-[sci_instr, cost_instr, m_instr, power_instr] = Instrumentation(Instr)
+[sci_instr, cost_instr, m_instr, power_instr] = Instrumentation(candidateArchitecture)
 
 %Calculate Telemetry Data Rate
 
