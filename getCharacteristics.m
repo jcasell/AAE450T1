2ef9447694d7fdd47f043ee28d6ea52408740311
ewill1904 <98923566+ewill1904@users.noreply.github.com>
2022-02-06 23:20:52 -0500

    Add files via upload
    Function to get orbital characteristics of various planets

---
+++ getCharacteristics.m
@@ -0,0 +1,20 @@
+function rad_list = getCharacteristics(options)
+%% Get Characteristics Function
+% This function will take in the trajectory options and return the orbit
+% radii 
+% 
+% Inputs: options - trajectory option
+%
+% Outputs: rad_list - vector of orbit radii for planets being used
+%
+%% Options
+switch options
+    case {"JupNep","JupNep_O"}
+        a1 = 778279959; %Jupiter [km]
+        a2 = 4498337290; %Neptune [km]
+    case {"JupSat","JupSat_O"}
+        a1 = 778279959; %Jupiter [km]
+        a2 = 1427387908; %Saturn [km]
+end
+rad_list = [a1,a2];
+end
