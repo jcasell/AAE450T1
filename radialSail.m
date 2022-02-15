6983616a18c6577dac0af00fb03d94471befb428
ConnerPhillips <97711549+ConnerPhillips@users.noreply.github.com>
2022-02-15 16:57:46 -0500

    Merged from SolarSailTest
    This code should work now for any beta value above 0.5 and give us a TOF

--- radialSail.m
+++ radialSail.m
@@ -1,35 +1,44 @@
-function tof = radialSail(r0,v0,rF,beta)
+function [vF,fpaF,tof] = radialSail(r0,v0,rF,beta)
 muSun = 132712440017.99; %km^3/s^2
 modifiedMu = muSun * (1 - beta);
 
-vEsc = sqrt(2 * modifiedMu / r0);
-fpa0 = 0; %Assume initial departure from solar periapsis
+% vEsc = sqrt(2 * modifiedMu / r0);
+% fpa0 = 0; %Assume initial departure from solar periapsis
 
-sma = 0.5 * (modifiedMu / ((modifiedMu / r0) - (v0^2 / 2))); %Calculate semimajor axis
-ecc = sqrt(((0.5 * r0 * v0^2)/modifiedMu - 1)^2 * cosd(fpa0)^2 + sind(fpa0)^2);
+% sma = 0.5 * (modifiedMu / ((modifiedMu / r0) - (v0^2 / 2))); %Calculate semimajor axis
+sma = r0*((1 - beta)/(1 - (2*beta)));
+% ecc = sqrt(((0.5 * r0 * v0^2)/modifiedMu - 1)^2 * cosd(fpa0)^2 + sind(fpa0)^2);
+ecc = 1 - ((1 - 2*beta)/(1 - beta));
 
-if v0 > vEsc %Orbit type is hyperbolic if velocity greater than escape
+if beta > 0.5 %Orbit type is hyperbolic if velocity greater than escape
     orbitType = "Hyperbolic";
 else
     orbitType = "Elliptic";
 end
 
-initialTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/r0 - 1)));
-finalTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1)));
+% initialTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/r0 - 1)));
+% finalTA = acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1));
 
 if orbitType == "Elliptic"
-    initialE = 2*atan2(tand(initialTA/2), sqrt((1 + ecc)/(1-ecc)));
+    % True Anomaly
+    finalTA = acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1));
+    % Eccentric Anomaly
     finalE = 2*atan2(tand(finalTA/2), sqrt((1 + ecc)/(1-ecc)));
-    if finalE < 0
-        finalE = finalE + 2*pi;
-    end
 
-    tof = sqrt(sma^3/modifiedMu) * (finalE - ecc*sin(finalE) - (initialE - ecc*sin(initialE)));
+    % TOF, Velocity, and Position 
+    tof = sqrt(sma^3/modifiedMu) * (finalE - ecc*sin(finalE));
+    vF = sqrt((2*modifiedMu/rF) - (modifiedMu/sma));
+    fpaF = atan2d(ecc*sind(finalTA),1 + ecc*cosd(finalTA));
 elseif orbitType == "Hyperbolic"
-    initialH = 2*atanh(tand(initialTA/2) / sqrt((ecc+1)/(ecc-1)));
+    % True Anomaly
+    finalTA = acosd(1 / ecc * (abs(sma)*(ecc^2 - 1)/rF - 1));
+    % Hyperbolic Anomaly
     finalH = 2*atanh(tand(finalTA/2) / sqrt((ecc+1)/(ecc-1)));
     
-    tof = sqrt(abs(sma)^3 / modifiedMu) * (ecc*sinh(finalH) - finalH - (ecc*sinh(initialH) - initialH));
+    % TOF, Velocity, and Position 
+    tof = sqrt(abs(sma)^3 / modifiedMu) * (ecc*sinh(finalH) - finalH);
+    vF = sqrt((2*modifiedMu/rF) + (modifiedMu/abs(sma)));
+    fpaF = atan2d(ecc*sind(finalTA),1 + ecc*cosd(finalTA));
 end
 
 tof = tof / (3600 * 24 * 365.25); %Convert time of flight from seconds to Julian years
