98c7a920abf02e10f711fdbfe5356e5a46398c63
ConnerPhillips <97711549+ConnerPhillips@users.noreply.github.com>
2022-02-15 17:03:16 -0500

    Updated Equations for Trajectory Types
    The equations to determine the TOF for the different types of conic sections are now fixed

--- detTof.m
+++ detTof.m
@@ -16,18 +16,24 @@ else
 end
 
 sma = 0.5 * (muSun / ((muSun / r0) - (v0^2 / 2))); %Calculate semimajor axis
-ecc = sqrt(((0.5 * r0 * v0^2)/muSun - 1)^2 * cosd(fpa0)^2 + sind(fpa0)^2);
+ecc = sqrt(((r0 * v0^2)/muSun - 1)^2 * cosd(fpa0)^2 + sind(fpa0)^2);
 
-initialTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/r0 - 1)));
-finalTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1)));
+% initialTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/r0 - 1)));
+% finalTA = abs(acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1)));
 
 if orbitType == "Elliptic"
+    initialTA = acosd(1 / ecc * (sma*(1 - ecc^2)/r0 - 1));
     initialE = 2*atan2(tand(initialTA/2), sqrt((1 + ecc)/(1-ecc)));
+
+    finalTA = acosd(1 / ecc * (sma*(1 - ecc^2)/rF - 1));
     finalE = 2*atan2(tand(finalTA/2), sqrt((1 + ecc)/(1-ecc)));
 
     tof = sqrt(sma^3/muSun) * (finalE - ecc*sin(finalE) - (initialE - ecc*sin(initialE)));
 elseif orbitType == "Hyperbolic"
+    initialTA = acosd(1 / ecc * (abs(sma)*(ecc^2 - 1)/r0 - 1));
     initialH = 2*atanh(tand(initialTA/2) / sqrt((ecc+1)/(ecc-1)));
+
+    finalTA = acosd(1 / ecc * (abs(sma)*(ecc^2 - 1)/rF - 1));
     finalH = 2*atanh(tand(finalTA/2) / sqrt((ecc+1)/(ecc-1)));
     
     tof = sqrt(abs(sma)^3 / muSun) * (ecc*sinh(finalH) - finalH - (ecc*sinh(initialH) - initialH));
