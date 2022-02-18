2b319032e2fae6a4ede3ed1461bc813050a237c8
austinjbarrow <57372430+austinjbarrow@users.noreply.github.com>
2022-02-17 21:33:20 -0500

    updated Block 1B and Block 2 C3 equations

--- curvefitLV.m
+++ curvefitLV.m
@@ -2,19 +2,16 @@
 clc;
 clear all;
 
-C3 = [0,5,10,15,20,25,30];
-Mass_V6S = [7180, 6360, 4930, 3605, 2365, 1205, 120];
-
-C3_sls = [-3 0   5 10 20 30 40 50 60 70 80 90 100 160];
-Payload_mass_sls = [28 27 24 22 18 15 13 11 9 7.5 6.5 5.25 4 1].*1000; %kg
+C3 = [0,10,20,30,40,50,60,70,80,83,90,100,110,120];
+Mass = [44.3, 37.6, 31.6, 26.4, 21.8, 17.9, 14.4, 11.4, 8.8, 8.0, 6.4, 4.4, 2.6, 0.9].*1000; %kg
 
 
-coefficients = polyfit(Payload_mass_sls, C3_sls, 3);
+coefficients = polyfit(Mass, C3, 3);
 
-xFit = linspace(min(Payload_mass_sls), max(Payload_mass_sls), 1000);
+xFit = linspace(min(Mass), max(Mass), 1000);
 yFit = polyval(coefficients , xFit);
 
-plot(Payload_mass_sls, C3_sls, 'b.', 'MarkerSize', 15);
+plot(Mass, C3, 'b.', 'MarkerSize', 15);
 hold on; % Set hold on so the next plot does not blow away the one we just drew.
 plot(xFit, yFit, 'r-', 'LineWidth', 2); % Plot fitted line.
 grid on;
