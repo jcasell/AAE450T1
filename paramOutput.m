f054f1aed2d3eaa9b026eb4c3697b2a0f47714ba
ewill1904 <will1904@purdue.edu>
2022-02-20 14:51:36 -0500

    Update code for GMAT outputs

--- paramOutput.m
+++ paramOutput.m
@@ -1,26 +1,33 @@
-function [paramList] = paramOutput(trajectoryOption, v_inf,deltaV)
+function [paramList] = paramOutput(trajectoryOption, v_inf)
 
 paramList = zeros(3,4);
 
+aEarth = 149597898; aJup = 778279959;
+aSat = 1427387908; aMars = 227956000; %Semimajor axes of Earth, Jupiter, Mars, and Saturn
+
+if trajectoryOption == "JupSat"
+    rList = [aEarth,aJup,aSat];
+    planetList = ["Jupiter","Saturn"];
+elseif trajectoryOption == "MarsJup"
+    rList = [aEarth,aMars,aJup];
+    planetList = ["Mars","Jupiter"];
+end
+
 muSun = 132712440017.99; % grav parameter of sun [km^3/s^2]
 
-aEarth = 149597898; aJup = 778279959; aSat = 1427387908; %Semimajor axes of Earth, Jupiter, and Saturn
 
 vEarth = sqrt(2*muSun/aEarth); %velocity of Earth relative to Sun [km/s]
 v0 = v_inf + vEarth; %initial velocity of s/c relative to sun [km/s]
 
-vEsc = sqrt(2*muSun/aEarth);
+currentV = v0; currentFPA = 0;
 
+for i = 1:2
+    [sma, ecc, ta0,taf] = getTA(rList(i),currentV,rList(i+1),currentFPA);
+    [vArrival, fpaArr] = getFPA(rList(i),currentV,rList(i+1),currentFPA);
+    [currentV,currentFPA] = gravityAssist(planetList{i},vArrival,fpaArr);
 
-if v0 >= vEsc
-    orbitType = "Hyperbolic";
-else
-    orbitType = "Elliptic";
-end
-
-for i = 1:3
-    [sma, ecc, ta0,taf] = getTA(aEarth,v0,aJup,0);
     paramList(i,:) = [sma,ecc,ta0,taf];
+    
 end
 
 disp(paramList);
