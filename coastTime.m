570e9faff1e7c463919c3d82103886dcddd43421
ewill1904 <will1904@purdue.edu>
2022-02-20 13:31:04 -0500

    Fixed bugs for Pareto

--- coastTime.m
+++ coastTime.m
@@ -18,4 +18,21 @@ phase2Time = detTof(phase1R,phase1V,phase2R,phase1Fpa);
 
 phase3Time = 35-phase2Time-phase1Time;
 phase3Dist = time2dist(phase2R,phase2V, phase3Time, phase2Fpa);
+
+if (phase3Dist/au2km) > 250
+    ENARad = 250*au2km; %dist for ENA
+    ENATime = detTof(phase2R,phase2V,ENARad,phase2Fpa);
+    ENATime = phase3Time - ENATime;
+else
+    ENATime = 0;
+end
+
+if (phase3Dist/au2km) > 300
+    LYARad = 300*au2km; %dist for LYA
+    LYATime = detTof(phase2R,phase2V,LYARad,phase2Fpa);
+    LYATime = phase3Time - LYATime;
+else
+    LYATime = 0;
+end
+
 totalTime = [phase1Time, phase2Time, phase3Time];
