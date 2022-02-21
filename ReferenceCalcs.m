3efd3b55b503be6d536d854a3bd4c4e18b059d07
jcasell <jcasell@purdue.edu>
2022-02-21 18:43:15 -0500

    Update ReferenceCalcs.m

--- ReferenceCalcs.m
+++ ReferenceCalcs.m
@@ -4,11 +4,11 @@ close all
 
 candidateArchitecture.Communications = "DSN";
 candidateArchitecture.Telemetry = "Ka";
-candidateArchitecture.Propulsion = "OMS";
+candidateArchitecture.Propulsion = "BHT-200";
 candidateArchitecture.Power = "RTG Nuclear";
 candidateArchitecture.Instruments = "Mid Level";
-candidateArchitecture.Trajectory = "JupSatO";
-candidateArchitecture.LaunchVehicle = "SLS Block 2";
+candidateArchitecture.Trajectory = "MarsJup";
+candidateArchitecture.LaunchVehicle = "Starship";
 candidateArchitecture.Kick = "Centaur V & Star 48BV";
 candidateArchitecture.num_Kick = 2;
 
