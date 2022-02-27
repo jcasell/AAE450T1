27c826cae197301ed2555c1b606e6df76fcb940a
austinjbarrow <57372430+austinjbarrow@users.noreply.github.com>
2022-02-27 17:42:43 -0500

    updated C3 graphs

--- C3Graphs.m
+++ C3Graphs.m
@@ -1,187 +1,191 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Script Name: GenerateC3
-%Description: generate final velocity
-%Author: Austin Barrow and Propulsion Team
+%Script Name: ParetoMaster
+%Description: Calculates and Plots Pareto for Analysis
+%Author: Jeremy Casella and Brendan Jones
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [final_v, invalid, added_V] = generateC3( candidateArchitecture, m_pay )
-    % Inputs: Launch Vehicle, Kick Stage Propulsion, C3 values (0,10,20,30,40,50,60,70,80,90,100, 110, 120)
-    % Outputs: Delta V, Delta V caused by Kick Stage, Mass Payload, Structural Mass, Mass Propellant
+clc
+clear
+close all
+tic
 
-    %% Setting Constants and Assumed Values (values to iterate over)
-  
-    array_LV = ["SLS Block 1", "SLS Block 1B", "SLS BLock 2", "Falcon Heavy", "Starship", "New Glenn", "Vulcan 6S"]; % Array of Launch Vehicles
-    kick_Stage = ["Star 48BV", "Centaur V", "Nuclear", "Hybrid", "Centaur V & Star 48BV", ...
-        "Centaur V & Nuclear", "Centaur V & Hybrid", "Star 48BV & Hybrid", "Star 48 BV & Nuclear", ...
-        "Hybrid & Nuclear", "Castor 30XL & Star 48BV", "Castor 30XL & Nuclear", "Castor 30XL & Hybrid","No Kick Stage"];
-    num_Kick = [0, 1, 2];
-    g_E = 9.81; % (m/s^2)
-    v_esc_E = 11200; % Escape velocity of Earth from LEO m/s
-    
-    %% Switch statement to determine assumed ISP (Impulse), Lambda (Payload 
-    % Fraction), Inert Mass Fraction for kick stage
+%Define Possible Matrix Options
+ComNet = ["DSN"]; %Communication Network Options
+Telem = ["Ka" "X" "S"];     %Telemetry Band
+Prop = ["Chemical" "Solar Sail" "BHT-200" "BHT-600"];   %Propulsion Options
+Power = ["RTG Nuclear"];  %Power Source Options
+Instr = ["Minimum" "Mid Level" "High Level"];    %Instrumentation Options
+Traj = ["JupSat" "JupSatO" "Log Spiral" "Solar Grav" "MarsJup"]; %Trajectory Options (O indicates impulse manuever during GA)
+LaunchV = ["SLS Block 2", "Falcon Heavy", "Starship", "Vulcan 6S"];    %Launch Vehicle Options
+Kick = ["Castor 30XL" "Star 48BV", "Centaur V", "Nuclear", "Hybrid", "Centaur V & Star 48BV", ...
+        "Centaur V & Nuclear", "Centaur V & Hybrid", "Star 48BV & Hybrid", "Star48 BV & Nuclear", ...
+        "Hybrid & Nuclear", "Castor 30XL & Star 48BV", "Castor 30XL & Nuclear", ...
+        "Castor 30XL & Hybrid", "No Kick Stage"];   %Kick Stages Options
+NumKick = [0 1 2];
 
-    switch candidateArchitecture.Kick 
-        case "Star 48BV" % Solid Rocket 
-            isp = 286;
-            lambda = 0.939;
-            m_kick1 = 2624 + m_pay;
-        case "Centaur V" %Liquid
-            isp = 451; % LH2/LOX
-            lambda = 0.91; % Centaur Kick Stage
-            m_kick1 = 22825 + m_pay;
-        case "Castor 30XL"
-            isp = 294;
-            lambda = .944;
-            m_kick1 = 26406.79 + m_pay;
-        case "Nuclear" % LOOK AT BMX TECHNOLOGIES
-            isp = 875; 
-            lambda = 0.74; 
-            m_kick1 = 2547.375 + m_pay;
-        case "Hybrid" % SNC Vortex Engine
-            isp = 325;
-            lambda = 0.875;
-            m_kick1 = 2200 + m_pay;
-        case "Centaur V & Star 48BV" %Liquid
-            isp1 = 451; % LH2/LOX
-            lambda1 = 0.91; % Centaur Kick Stage
-            isp2 = 286; %Star 48BV
-            lambda2 = 0.939;
-            m_kick2 = 2624 + m_pay;
-            m_kick1 = 22825 + m_kick2; %Centaur Kick Stage hauling Star48BV
-        case "Centaur V & Nuclear" % LOOK AT BMX TECHNOLOGIES
-            isp1 = 451; % LH2/LOX
-            lambda1 = 0.91; % Centaur Kick Stage
-            isp2 = 875; 
-            lambda2 = 0.74; 
-            m_kick2 = 2547.375 + m_pay; %needs to be updated, nuclear
-            m_kick1 = 22825 + m_kick2; %Centaur Kick Stage hauling Nuclear
-        case "Centaur V & Hybrid" % LOOK AT SIERRA NEVADA
-            isp1 = 451; % LH2/LOX
-            lambda1 = 0.91; % Centaur Kick Stage
-            isp2 = 325;
-            lambda2 = 0.875; 
-            m_kick2 = 2200 + m_pay; %needs to be updated, hybrid
-            m_kick1 = 22825 + m_kick2; %Centaur Kick Stage hauling Hybrid
-        case "Castor 30XL & Star 48BV" %Liquid
-            isp1 = 294;
-            lambda1 = 0.944; 
-            isp2 = 286; %Star 48BV
-            lambda2 = 0.939;
-            m_kick2 = 2624 + m_pay;
-            m_kick1 = 26406.79 + m_kick2; %Castor 30XL Kick Stage hauling Star48BV
-        case "Castor 30XL & Nuclear" % LOOK AT BMX TECHNOLOGIES
-            isp1 = 294;
-            lambda1 = 0.944;
-            isp2 = 875; 
-            lambda2 = 0.74; 
-            m_kick2 = 2547.375 + m_pay; %needs to be updated, nuclear
-            m_kick1 = 26406.79 + m_kick2; %Castor 30XL Kick Stage hauling Nuclear
-        case "Castor 30XL & Hybrid" % LOOK AT SIERRA NEVADA
-            isp1 = 294;
-            lambda1 = 0.944;
-            isp2 = 325;
-            lambda2 = 0.875; 
-            m_kick2 = 2200 + m_pay; %needs to be updated, hybrid
-            m_kick1 = 26406.79 + m_kick2; %Castor 30XL Kick Stage hauling Hybrid
-         case "Star 48BV & Hybrid" % Solid Rocket 
-            isp1 = 286;
-            lambda1 = 0.939;
-            isp2 = 325;
-            lambda2 = 0.875;
-            m_kick2 = 2200 + m_pay; %hybrid
-            m_kick1 = 2624 + m_kick2;
-        case "Star 48BV & Nuclear" % Solid Rocket 
-            isp2 = 875;
-            lambda2 = 0.74;
-            isp1 = 286;
-            lambda1 = 0.939;
-            m_kick2 = 2547.375 + m_pay; %needs to be updated, nuclear
-            m_kick1 = 2624 + m_kick2;
-        case "Hybrid & Nuclear" % Hybrid & Nuclear
-            isp1 = 875; % nuclear
-            lambda1 = 0.74; % nuclear
-            isp2 = 325; % hybrid
-            lambda2 = 0.875; % Hybrid Kick Stage
-            m_kick2 = 2200 + m_pay; %needs to be updated, nuclear
-            m_kick1 = 2547.375 + m_kick2; %Centaur Kick Stage hauling Hybrid
-        otherwise % No kick stage
-            m_kick1 = m_pay;
-            
+%Create Results Table
+ResultsRaw = [];
+Results10Raw = [];
+PointColor = [];
+PointColor10 = [];
+
+%Create Permutations of Missions
+for i1 = ComNet
+    %Only Include Relevant Combination
+    if i1 == "DSN"
+        TelemAllow = ["Ka"];
+    else
+        TelemAllow = Telem;
     end
 
-    %% Switch statement to determine C3 (km^2/s^2) at given payload mass
-    % Uses equations from curvefit of C3 vs mass
-    switch candidateArchitecture.LaunchVehicle
-        case "SLS Block 1"
-            C3 = -1.36254651049771e-11 * (m_kick1)^3 + ...
-                8.34210950132914e-07 * (m_kick1)^2 + -0.0189976032356646 ...
-                * m_kick1 + 172.041984919931;
-        case "SLS Block 1B"
-            C3 = -1.15085276593111e-12 * (m_kick1)^3 + ...
-                1.18020100654521e-07 * (m_kick1)^2 + ...
-                -0.00587448959766228 * m_kick1 + 119.768784427882;
-        case "SLS Block 2"
-            C3 = -1.10638439713243e-12 * (m_kick1)^3 + ...
-                1.21222227649818e-07 * (m_kick1)^2 + ...
-                -0.00602174622627308 * m_kick1 + 124.597075687923;
-        case "Falcon Heavy Recoverable"
-            C3 = -6.19433969053741e-11 * (m_kick1)^3 + ...
-                1.348688172148435e-06 * (m_kick1)^2 + -0.0160 * m_kick1 ...
-                + 64.84;
-%         case "Falcon Heavy Expendable"
-%             C3 = -2.202623249080307e-11 * (m_kick)^3 + ...
-%                 8.680612077815912e-07 * (m_kick)^2 + -0.0155 * m_kick ...
-%                 + 110.4074;
-        case "Starship" 
-            C3 = (0.0047^2 * (log(1500 / (5 * (m_kick1 / 1000)+120) ) )^2 - 0.0032^2 ) ...
-                *1E6;
-        case "New Glenn"
-            C3 = -4.42248334552639e-11 * (m_kick1)^3 + ...
-                4.98373417329502e-07 * (m_kick1)^2 + -0.0055 * m_kick1 ...
-                + 30.7952;
-        case "Vulcan 6S"
-            C3 = -6.89376184059982e-11 * (m_kick1)^3 + ...
-                1.82960892594491e-06 * (m_kick1)^2 + -0.0225 * m_kick1 ...
-                + 116.117;
-        otherwise 
-            C3 = -1;
+    for i2 = TelemAllow
+        for i3 = Prop
+
+            if i3 == "Chemical"
+                TrajAllow = ["JupSatO"];
+            elseif i3 == "Solar Sail"
+                TrajAllow = ["Log Spiral" "Solar Grav"];
+            else
+                TrajAllow = ["JupSat" "MarsJup"];
+            end
+
+            for i4 = Power
+                %Only Include Relevant Combination
+                if i4 == "Solar Panel"
+                    InstrAllow = ["Minimum"];
+                else
+                    InstrAllow = Instr;
+                end
+                for i5 = InstrAllow
+                    for i6 = TrajAllow
+                        for i7 = LaunchV
+                            for i8 = NumKick
+                                if i8 == 0;
+                                    KickAllow = ["No Kick Stage"];
+                                elseif i8 == 1;
+                                    KickAllow = ["Star 48BV","Centaur V","Nuclear","Hybrid", "Castor 30XL"];
+                                elseif i8 == 2;
+                                    KickAllow = ["Centaur V & Star 48BV","Centaur V & Nuclear","Centaur V & Hybrid","Star 48BV & Hybrid","Star 48BV & Nuclear","Hybrid & Nuclear", "Castor 30XL & Star 48BV", "Castor 30XL & Nuclear", "Castor 30XL & Hybrid"];
+                                end
+                                for i9 = KickAllow
+                                    candidateArchitecture.Communications = i1;
+                                    candidateArchitecture.Telemetry = i2;
+                                    candidateArchitecture.Propulsion = i3;
+                                    candidateArchitecture.Power = i4;
+                                    candidateArchitecture.Instruments = i5;
+                                    candidateArchitecture.Trajectory = i6;
+                                    candidateArchitecture.LaunchVehicle = i7;
+                                    candidateArchitecture.num_Kick = i8;
+                                    candidateArchitecture.Kick = i9;
+                              
+                                    %Call Mission Program
+                                    [science, cost, reliability, ttHP, invalid] = MissionCalc(candidateArchitecture);
+
+                                    cost = cost*1.3;
+                                    
+                                    if invalid == false
+                                        %Create Table of Results etc
+                                        ResultsRaw = [ResultsRaw; [i1 i2 i3 i4 i5 i6 i7 i8 i9 cost science reliability ttHP]];
+                                        
+                                        if ttHP <= 11;
+                                            if ttHP <= 10;
+                                                PointColor = [PointColor;0 1 0];
+                                                PointColor10 = [PointColor10;0 1 0];
+                                            else
+                                                PointColor = [PointColor;0 0 1];
+                                                PointColor10 = [PointColor10;0 0 1];
+                                            end
+                                            Results10Raw = [Results10Raw; [i1 i2 i3 i4 i5 i6 i7 i8 i9 cost science reliability ttHP]];
+                                        else
+                                            PointColor = [PointColor;1 0 0];
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
-    
-    invalid = false;
-    if(C3 < 0) 
-        invalid = true;
-        final_v = 0;
-        added_V = 0;
-        return
-    end 
-
-    %% Calculations     
-    if(candidateArchitecture.num_Kick == 1)        
-        % Calculation mass propellant, inert mass, and Mass Ratio
-        m_prop = (m_kick1 - m_pay) * lambda;
-        m_inert = (m_kick1 - m_pay - m_prop); % Structural mass of the kick stage (Mass minus final payload and propellant)
-        MR = (m_pay + m_prop + m_inert) / (m_pay + m_inert); % Mass Ratio
-
-        % Calculation of Velocity Infinite with rocket equation (km/s)
-        added_V = g_E * isp * log(MR); 
-    elseif(candidateArchitecture.num_Kick == 2)
-        %Stage 1:
-        m_prop1 = (m_kick1 - m_kick2) * lambda1;
-        m_inert1 = (m_kick1 - m_kick2 - m_prop1); 
-        MR = (m_kick2 + m_prop1 + m_inert1) / (m_kick2 + m_inert1); 
-
-        added_V = g_E * isp1 * log(MR); 
-        
-        %Stage 2:
-        m_prop2 = (m_kick2 - m_pay) * lambda2;
-        m_inert2 = (m_kick2 - m_pay - m_prop2); 
-        MR = (m_pay + m_prop2 + m_inert2) / (m_pay + m_inert2); 
-
-        added_V = added_V + g_E * isp2 * log(MR);
-    else %num_kick = 0
-        added_V = 0;
-    end
-    
-    final_v = added_V/1000 + sqrt(C3+(v_esc_E/1000)^2);
 end
+
+%Parse Table
+Results = array2table(ResultsRaw,'VariableNames', {'Communications','Telemetry','Propulsion','Power','Instruments','Trajectory','Launch_Vehicle','Number_Kick','Kick_Stages','Cost','Science','Reliability','TT_Heliopause'})
+Results10= array2table(Results10Raw,'VariableNames', {'Communications','Telemetry','Propulsion','Power','Instruments','Trajectory','Launch_Vehicle','Number_Kick','Kick_Stages','Cost','Science','Reliability','TT_Heliopause'})
+
+Results.Cost = double(Results.Cost);
+Results.Science = double(Results.Science);
+Results.Reliability = double(Results.Reliability);
+Results.TT_Heliopause = double(Results.TT_Heliopause);
+
+Results10.Cost = double(Results10.Cost);
+Results10.Science = double(Results10.Science);
+Results10.Reliability = double(Results10.Reliability);
+Results10.TT_Heliopause = double(Results10.TT_Heliopause);
+
+
+%Add Data Tips
+figure(1)
+s = scatter(Results.Cost,Results.Science,[],PointColor);
+xlabel('System Cost (F2022 Millions of Dollars)')
+ylabel('Science Value')
+title('Science Value vs Cost Pareto Frontier')
+
+row = dataTipTextRow('Cost',Results.Cost);
+s.DataTipTemplate.DataTipRows(1) = row;
+row = dataTipTextRow('Science',Results.Science);
+s.DataTipTemplate.DataTipRows(2) = row;
+row = dataTipTextRow('Communications',Results.Communications);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Telemetry',Results.Telemetry);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Propulsion',Results.Propulsion);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Power',Results.Power);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Instruments',Results.Instruments);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Trajectory',Results.Trajectory);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Launch Vehicle',Results.Launch_Vehicle);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Number of Kick Stages',Results.Number_Kick);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Kick Stages',Results.Kick_Stages);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Time to Heliopause',Results.TT_Heliopause);
+s.DataTipTemplate.DataTipRows(end+1) = row;
+
+figure(2)
+s2 = scatter(Results10.Cost,Results10.Science,[],PointColor10);
+xlabel('System Cost (F2022 Millions of Dollars)')
+ylabel('Science Value')
+title('Viable Results Science Value vs Cost Pareto Frontier')
+
+row = dataTipTextRow('Cost',Results10.Cost);
+s2.DataTipTemplate.DataTipRows(1) = row;
+row = dataTipTextRow('Science',Results10.Science);
+s2.DataTipTemplate.DataTipRows(2) = row;
+row = dataTipTextRow('Communications',Results10.Communications);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Telemetry',Results10.Telemetry);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Propulsion',Results10.Propulsion);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Power',Results10.Power);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Instruments',Results10.Instruments);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Trajectory',Results10.Trajectory);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Launch Vehicle',Results10.Launch_Vehicle);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Number of Kick Stages',Results10.Number_Kick);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Kick Stages',Results10.Kick_Stages);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+row = dataTipTextRow('Time to Heliopause',Results10.TT_Heliopause);
+s2.DataTipTemplate.DataTipRows(end+1) = row;
+
+toc
