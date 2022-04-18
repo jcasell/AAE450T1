3bee711b6bbc0ae308e216d3835b02c0251316ab
jcasell <jcasell@purdue.edu>
2022-04-18 18:37:16 -0400

    Cost Adjust

--- CostComponentCalcs.m
+++ CostComponentCalcs.m
@@ -9,23 +9,14 @@ power_spacecraft = power_instr / 0.22;
 %All Costs in F2022 Millions
 
 %% Instrument Cost and See
-%Old
-MAG_cost = 10.43; MAG_sdev = 0.28*MAG_cost;
-PLS_cost = 23.3; PLS_sdev = 0.29*PLS_cost;
-PUI_cost = 17.09; PUI_sdev = 0.29*PUI_cost;
-PWS_cost = 18.34; PWS_sdev = 0.29*PWS_cost;
-EPS_cost = 13.97; EPS_sdev = 0.29*EPS_cost;
-CRS_cost = 18.88; CRS_sdev = 0.29*CRS_cost;
-ENA_cost = 28.95; ENA_sdev = 0.29*ENA_cost;
-
-%Possible New Restructured Cost, limited life to 15 for non ENA, ENA is 35
-% MAG_cost = 6.43; MAG_sdev = 0.28*MAG_cost;
-% PLS_cost = 20.16; PLS_sdev = 0.29*PLS_cost;
-% PUI_cost = 14.79; PUI_sdev = 0.29*PUI_cost;
-% PWS_cost = 15.86; PWS_sdev = 0.29*PWS_cost;
-% EPS_cost = 12.09; EPS_sdev = 0.29*EPS_cost;
-% CRS_cost = 16.33; CRS_sdev = 0.29*CRS_cost;
-% ENA_cost = 25.17; ENA_sdev = 0.29*ENA_cost;
+% New Restructured Cost, limited life to 15 for non ENA, ENA is 35
+MAG_cost = 6.43; MAG_sdev = 0.28*MAG_cost;
+PLS_cost = 20.16; PLS_sdev = 0.29*PLS_cost;
+PUI_cost = 14.79; PUI_sdev = 0.29*PUI_cost;
+PWS_cost = 15.86; PWS_sdev = 0.29*PWS_cost;
+EPS_cost = 12.09; EPS_sdev = 0.29*EPS_cost;
+CRS_cost = 16.33; CRS_sdev = 0.29*CRS_cost;
+ENA_cost = 25.17; ENA_sdev = 0.29*ENA_cost;
 
 %Vector For Cost
 costInstvec = [MAG_cost,PLS_cost,PUI_cost,PWS_cost,EPS_cost,CRS_cost,ENA_cost];
@@ -48,6 +39,7 @@ ADCScost = costAttNRec+costAttRec
 
 %Electrical Cost
 costElecRec = 2*108
+nuclearLaunch = 1.10 * 40.3
 
 %Communications Cost
 costCommNRec = 1.29*(618*(m_spacecraft*0.07))/10^3;
@@ -96,5 +88,5 @@ opsCostexp = (1.29*1*(techCount*150+engCount*200) + 1.29*34*(techCount_cut*150+e
 costICHot = 5.72
 
 %% Total Cost Calculation
-cost = costVehNRec+costVehRec+costProg+LOOScost+AGEcost+opsCostexp+costDSN +costICHot
+cost = costVehNRec+costVehRec+costProg+nuclearLaunch+LOOScost+AGEcost+opsCostexp+costDSN +costICHot
 costMargin = 1.3*cost
--- MonteCarloCost.m
+++ MonteCarloCost.m
@@ -8,22 +8,14 @@ power_spacecraft = power_instr / 0.22;
 %All Costs in F2022 Millions
 
 %% Instrument Cost and See
-MAG_cost = 10.43; MAG_sdev = 0.28*MAG_cost;
-PLS_cost = 23.3; PLS_sdev = 0.29*PLS_cost;
-PUI_cost = 17.09; PUI_sdev = 0.29*PUI_cost;
-PWS_cost = 18.34; PWS_sdev = 0.29*PWS_cost;
-EPS_cost = 13.97; EPS_sdev = 0.29*EPS_cost;
-CRS_cost = 18.88; CRS_sdev = 0.29*CRS_cost;
-ENA_cost = 28.95; ENA_sdev = 0.29*ENA_cost;
-
-%Possible New Restructured Cost, limited life to 15 for non ENA, ENA is 35
-% MAG_cost = 6.43; MAG_sdev = 0.28*MAG_cost;
-% PLS_cost = 20.16; PLS_sdev = 0.29*PLS_cost;
-% PUI_cost = 14.79; PUI_sdev = 0.29*PUI_cost;
-% PWS_cost = 15.86; PWS_sdev = 0.29*PWS_cost;
-% EPS_cost = 12.09; EPS_sdev = 0.29*EPS_cost;
-% CRS_cost = 16.33; CRS_sdev = 0.29*CRS_cost;
-% ENA_cost = 25.17; ENA_sdev = 0.29*ENA_cost;
+% New Restructured Cost, limited life to 15 for non ENA, ENA is 35
+MAG_cost = 6.43; MAG_sdev = 0.28*MAG_cost;
+PLS_cost = 20.16; PLS_sdev = 0.29*PLS_cost;
+PUI_cost = 14.79; PUI_sdev = 0.29*PUI_cost;
+PWS_cost = 15.86; PWS_sdev = 0.29*PWS_cost;
+EPS_cost = 12.09; EPS_sdev = 0.29*EPS_cost;
+CRS_cost = 16.33; CRS_sdev = 0.29*CRS_cost;
+ENA_cost = 25.17; ENA_sdev = 0.29*ENA_cost;
 
 %Vector For Cost
 instMu = [MAG_cost,PLS_cost,PUI_cost,PWS_cost,EPS_cost,CRS_cost,ENA_cost];
@@ -31,7 +23,7 @@ instSigma = [MAG_sdev,PLS_sdev,PUI_sdev,PWS_sdev,EPS_sdev,CRS_sdev,ENA_sdev];
 costInst = 0;
 
 %Instrument Cost Calculation
-for i = length(instMu)
+for i = 1:length(instMu)
     costInst = costInst + normrnd(instMu(i),instSigma(i));
 end
 
@@ -53,6 +45,7 @@ sdevAttRec = 0.36*costAttRec;
 costElecRec = 2*108;
 costElecMax = 2*118;
 costElecMin = 2*93;
+nuclearLaunch = 1.10 * 40.3;
 elecDist = makedist('Triangular','A',costElecMin,'B',costElecRec,'C',costElecMax);
 
 %Communications Cost
@@ -115,10 +108,10 @@ opsCost = random(opsDist);
 %Additional Objective Cost
 %IC-HOT
 ICmass = 24;
-ICbusRec = (1064+35.5*(ICmass)^1.261;
+ICbusRec = 1064+35.5*(ICmass)^1.261;
 ICbusSDEV = 3696;
 
-ICbus = normrnd(ICbusRec,ICbusSDEV)*1.29/10^3;
+ICbus = normrnd(ICbusRec,ICbusSDEV)*1.29*10^-3;
 ICpay = 0.4*ICbus;
 ICiat = 0.139*ICbus;
 ICprog = 0.229*ICbus;
@@ -128,6 +121,6 @@ ICcost = ICbus+ICpay+ICiat+ICprog+ICloos+ICgse;
 
 
 %% Total Cost Calculation
-cost = (costVehNRec+costVehRec+costProg+LOOScost+AGEcost+opsCost+costDSN+ICcost);
+cost = (costVehNRec+costVehRec+costProg+nuclearLaunch+LOOScost+AGEcost+opsCost+costDSN+ICcost);
 
 end
